7f1a2e91e45c6661ae0415497f17c033f4d75a0b
oyega <oyegaga02@gmail.com>
2022-07-05 12:31:39 +0100

    0-putchar.c is a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,13 @@
-#include "main.h"
+#include <stdio.h>
+
 
 /**
- * main - prints _putchar
+ * main -Entry point of the program
  *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
+	printf("_putchar\n");
 	return (0);
 }
