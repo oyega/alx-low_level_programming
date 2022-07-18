f7bc1e09e709691a379fc16f29f5f76f2478c0fe
oyega <oyegaga02@gmail.com>
2022-07-18 15:49:05 +0100

    2-strchr.c is a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,25 @@
-/**
-* _strspn - Gets the length of a prefix substring.
-* @s: String where substring will look.
-* @accept: Substring of accepted chars.
-* Return: Length of occurrence.
-*/
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int c = 0;
-	char *t = accept;
+#include "main.h"
 
-	while (*s++)
+/**
+ * _strchr - prints found c
+ * @s: pointer to char
+ * @c: char params to found
+ * Return: *S
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
 	{
-		while (*accept++)
-			if (*(s - 1) == *(accept - 1))
-			{
-				c++;
-				break;
-			}
-		if (!(*--accept))
-			break;
-		accept = t;
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
-	return (c);
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
 }
