3b4564e1a6f73a2eede0b4e2a72ad7342ae08c34
oyega <oyegaga02@gmail.com>
2022-07-28 16:55:24 +0100

    101-mul.c is a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,13 +17,12 @@ int i, j;
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
+			if (argv[i][j] > 57 || argv[i][j] < 488)
 			{  printf("Error\n");
 			exit(98); }
 		}
-
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
